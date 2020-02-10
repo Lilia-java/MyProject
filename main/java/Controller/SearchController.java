559dc8d1d1bbf705ca7423d6b03ddab531b53863
Lilia-java <57389049+Lilia-java@users.noreply.github.com>
2020-02-11 00:09:37 +0300

    first commit in my project

---
+++ main/java/Controller/SearchController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import Model.SearchModel;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+    @Controller
+    public class SearchController {
+        @GetMapping("/search")
+        public String searchingForm(Model model) {
+            //model.addAttribute("greeting", new Greeting());
+            model.addAttribute("search", new SearchModel());
+            return "search";
+        }
+        /*@PostMapping("/gresearch")
+        public String gettingInfo(@ModelAttribute SearchModel searchModel) {
+            if (searching.searchInfor()!=null){
+                return "/search";}
+            return "/unsearch";
+        }*/
+
+
+    }
+
+
